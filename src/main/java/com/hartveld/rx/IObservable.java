fe73b65b6dab4df83a44fd8201a5e9286d1e5108
David Hartveld <david@hartveld.com>
2013-01-10 01:11:39 +0100

    Implemented operator: observeOn(ExecutorService).

--- src/main/java/com/hartveld/rx/IObservable.java
+++ src/main/java/com/hartveld/rx/IObservable.java
@@ -1,7 +1,26 @@
 package com.hartveld.rx;
 
+import java.util.concurrent.ExecutorService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public interface IObservable<T> {
 
+	static final Logger LOG = LoggerFactory.getLogger(IObservable.class);
+
 	AutoCloseable subscribe(Procedure1<T> onNext, Procedure1<Throwable> onError, Procedure onCompleted);
+
+	default IObservable<T> observeOn(ExecutorService svc) {
+		return new IObservable<T>() {
+			@Override public AutoCloseable subscribe(Procedure1<T> onNext, Procedure1<Throwable> onError, Procedure onCompleted) {
+				Procedure1<T> scheduledOnNext = e -> svc.execute(() -> onNext.procedure(e));
+				Procedure1<Throwable> scheduledOnError = e -> svc.execute(() -> onError.procedure(e));
+				Procedure scheduledOnCompleted = () -> svc.execute(() -> onCompleted.procedure());
+
+				final AutoCloseable ac = IObservable.this.subscribe(scheduledOnNext, scheduledOnError, scheduledOnCompleted);
+				return () -> ac.close();
+			}
+		};
+	}
 
 }
--- src/test/java/com/hartveld/rx/SubjectObserverTest.java
+++ src/test/java/com/hartveld/rx/SubjectObserverTest.java
@@ -1,9 +1,13 @@
 package com.hartveld.rx;
 
 import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.fail;
 import static org.junit.Assert.assertThat;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -12,10 +16,10 @@ public class SubjectObserverTest {
 	private static final String hello = "Hello";
 	private static final String world = "world";
 
-	boolean gotHello;
-	boolean gotWorld;
+	private boolean gotHello;
+	private boolean gotWorld;
 
-	boolean completed;
+	private boolean completed;
 
 	@Before
 	public void setUp() {
@@ -44,6 +48,46 @@ public class SubjectObserverTest {
 			e -> fail("Caught exception: " + e.getMessage()),
 			() -> { completed = true; }
 		);
+
+		subscription.close();
+
+		assertThat("Did not find hello", gotHello, is(true));
+		assertThat("Did not find world", gotWorld, is(true));
+		assertThat("Did not complete", completed, is(true));
+	}
+
+	@Test
+	public void testThatObserverIsNotifiedOnBackgroundThreadWithObserveOn() throws Exception {
+		long mainThreadId = Thread.currentThread().getId();
+		ExecutorService svc = Executors.newFixedThreadPool(1);
+		IObservable<String> source = Observables.observableOf(hello, world);
+		AutoCloseable subscription = source.observeOn(svc).subscribe(
+			text -> {
+				switch(text) {
+				case hello:
+					long observingThreadId0 = Thread.currentThread().getId();
+					assertThat("Observation 'element' is not on other thread: ", observingThreadId0, is(not(mainThreadId)));
+					gotHello = true;
+					break;
+				case world:
+					long observingThreadId1 = Thread.currentThread().getId();
+					assertThat("Observation 'element' is not on other thread: ", observingThreadId1, is(not(mainThreadId)));
+					gotWorld = true;
+					break;
+				default:
+					fail("Got unknown element: " + text);
+				}
+			},
+			e -> fail("Caught exception: " + e.getMessage()),
+			() -> {
+				long observingThreadId2 = Thread.currentThread().getId();
+				assertThat("Observation 'completed' is not on other thread: ", observingThreadId2, is(not(mainThreadId)));
+				completed = true;
+			}
+		);
+
+		svc.shutdown();
+		assertThat("ExecutorService tasks failed to terminate", svc.awaitTermination(1, TimeUnit.SECONDS), is(true));
 
 		subscription.close();
 
